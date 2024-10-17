94742ebe186b57ef99f993b5ac9755c6040ef80b
Fran314 <francesco.ghog@gmail.com>
2024-10-17 10:32:04 +0200

    clicking outside closes menu

--- components/MobileNavbar.vue
+++ components/MobileNavbar.vue
@@ -5,16 +5,12 @@ const active = ref(false)
 </script>
 <template>
     <div class="mobile-navbar-wrapper" :class="{ active }">
+        <div class="menu-wrapper">
+            <div class="hitbox" @click="active = !active" />
+            <div class="menu">Menu</div>
+        </div>
         <div class="main-navbar">
             <Navbar :page="page" />
-        </div>
-        <div class="footer">
-            <!-- <div class="mobile-time-and-gil"> -->
-            <!--     <TimeAndGil /> -->
-            <!-- </div> -->
-            <div class="menu-wrapper">
-                <div class="menu" @click="active = !active">Menu</div>
-            </div>
         </div>
     </div>
 </template>
@@ -27,63 +23,55 @@ const active = ref(false)
 
     padding: 1rem;
     display: flex;
-    flex-direction: column;
+    flex-direction: column-reverse;
     align-items: end;
     justify-content: end;
 
     gap: 2rem;
 
     pointer-events: none;
+
+    position: relative;
 }
 
 .mobile-navbar-wrapper.active {
     background: rgba(0, 0, 0, 0.6);
-
-    pointer-events: auto;
 }
 .main-navbar {
     width: 10rem;
-
     pointer-events: auto;
-
     display: none;
-
-    z-index: 10000;
 }
 .mobile-navbar-wrapper.active .main-navbar {
     display: block;
 }
 
-.footer {
-    width: 100%;
-    display: flex;
-    flex-direction: row-reverse;
-    justify-content: space-between;
-    align-items: end;
-    /* box-shadow: 0 0 30px 40px #1a1a1a; */
-    /* background: #1a1a1a; */
-}
-.mobile-time-and-gil {
-    border-radius: 10px;
-    /* box-shadow: 0 0 30px 50px #1a1a1a; */
-    /* background: #1a1a1a; */
-}
 .menu-wrapper {
     border-radius: 10px;
     border: 2px #fff solid;
     height: fit-content;
 
-    pointer-events: auto;
-
-    display: flex;
-    flex-direction: row-reverse;
+    /* pointer-events: auto; */
 
     box-shadow: 0 0 5px 0px #1a1a1a;
-    /* box-shadow: 0 0 30px 50px #1a1a1a; */
-    /* background: #1a1a1a; */
-}
-.menu {
+
     position: relative;
+}
+.mobile-navbar-wrapper.active .menu-wrapper {
+    position: initial;
+}
+.hitbox {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+
+    pointer-events: auto;
+    cursor: pointer;
+}
+
+.menu {
     padding: 0.5rem 2rem;
     background-color: var(--gray-2);
     border-top: 2px var(--gray-1) solid;
@@ -93,8 +81,5 @@ const active = ref(false)
     border-radius: 8px;
     display: flex;
     justify-content: center;
-    cursor: pointer;
-
-    /* font-size: smaller; */
 }
 </style>
